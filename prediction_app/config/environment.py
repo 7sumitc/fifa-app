3a2d103ca9efd6e1e6d9fb3b35292ac44313e287
7sumitc <7.sumitcr@gmail.com>
2026-05-31 07:23:42 +0000

    Fix: Adding Base URL

--- prediction_app/config/environment.py
+++ prediction_app/config/environment.py
@@ -3,6 +3,9 @@ import os
 
 load_dotenv()
 
-BASE_URL = os.getenv("API_BASE_URL")
-API_KEY = os.getenv("API_FOOTBALL_KEY")
+BASE_URL = os.getenv(
+    "API_BASE_URL",
+    "https://v3.football.api-sports.io"
+)
 
+API_KEY = os.getenv("API_FOOTBALL_KEY")
