4797080707e7986b50c831c892f9c782ee9e91ea
7sumitc <7.sumitcr@gmail.com>
2026-05-31 07:20:51 +0000

    Fix: Remove DB secrets method

--- prediction_app/config/environment.py
+++ prediction_app/config/environment.py
@@ -3,30 +3,6 @@ import os
 
 load_dotenv()
 
-BASE_URL = os.getenv(
-    "API_BASE_URL",
-    "https://v3.football.api-sports.io"
-)
-
+BASE_URL = os.getenv("API_BASE_URL")
 API_KEY = os.getenv("API_FOOTBALL_KEY")
 
-if not API_KEY:
-    try:
-        from dbruntime.dbutils import DBUtils
-        from pyspark.sql import SparkSession
-
-        spark = SparkSession.getActiveSession()
-        dbutils = DBUtils(spark)
-
-        API_KEY = dbutils.secrets.get(
-            scope="football-api",
-            key="api-football-key"
-        )
-
-    except Exception as e:
-        raise ValueError(
-            f"Unable to retrieve API key: {e}"
-        )
-
-if not API_KEY:
-    raise ValueError("API_FOOTBALL_KEY not found")
