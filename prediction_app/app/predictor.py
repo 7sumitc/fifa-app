077468e411c3c1ef7efc925ec864f678fea3cd63
7sumitc <7.sumitcr@gmail.com>
2026-06-04 18:22:44 +0000

    Fix: Updated predictor to return error

--- prediction_app/app/predictor.py
+++ prediction_app/app/predictor.py
@@ -4,37 +4,32 @@ from databricks.sdk import WorkspaceClient
 
 
 def get_teams():
+    try:
+        w = WorkspaceClient()
 
-    w = WorkspaceClient()
+        table_name = os.getenv("UC_TABLE_NAME")
+        warehouse_id = os.getenv("DATABRICKS_WAREHOUSE_ID")
 
-    table_name = os.getenv("UC_TABLE_NAME")
-    warehouse_id = os.getenv("DATABRICKS_WAREHOUSE_ID")
+        response = (
+            w.statement_execution.execute_statement(
+                warehouse_id=warehouse_id,
+                statement=f"""
+                SELECT DISTINCT home_team AS team
+                FROM {table_name}
+                """
+            )
+        )
 
-    response = w.statement_execution.execute_statement(
-        warehouse_id=warehouse_id,
-        statement=f"""
-        SELECT DISTINCT home_team AS team
-        FROM {table_name}
-
-        UNION
-
-        SELECT DISTINCT away_team AS team
-        FROM {table_name}
-
-        ORDER BY team
-        """
-    )
-
-    rows = response.result.data_array
-
-    return [row[0] for row in rows]
+        rows = response.result.data_array
+        return [row[0] for row in rows]
+    except Exception as e:
+        return [str(e)]
 
 
 def get_prediction(
     home_team: str,
     away_team: str
 ):
-
     w = WorkspaceClient()
 
     table_name = os.getenv("UC_TABLE_NAME")
@@ -52,12 +47,9 @@ def get_prediction(
     )
 
     rows = response.result.data_array
-
     if not rows:
         return "No Prediction"
-
     result = rows[0][0]
-
     mapping = {
         "H": "Home Win",
         "D": "Draw",
